2239363f5ea0a127f96002f47075aef607f700e9
Sam Svindland <sam@Sams-MBP.lan>
2022-11-23 20:42:09 -0600

    remove unused imports

--- FitnessApi/Controllers/VersionController.cs
+++ FitnessApi/Controllers/VersionController.cs
@@ -1,21 +1,17 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
-using FitnessServices.Models;
 using FitnessServices.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace FitnessApi.Controllers;
 
 public sealed class VersionController
 {
     private readonly IAuthService _authService;
-    
+
     public VersionController(IAuthService authService)
     {
         _authService = authService;
@@ -28,15 +24,14 @@ public sealed class VersionController
         ILogger log)
     {
         var authed = await _authService.CheckAuth(req);
-        
+
         if (authed == false)
         {
             return new UnauthorizedResult();
         }
 
         const int minVersion = 24;
-        
+
         return new OkObjectResult(minVersion);
     }
-    
 }
