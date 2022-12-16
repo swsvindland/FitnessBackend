d83b9997b373ae7bbc30db34b3bdbf27c2ac6bee
Sam Svindland <sam@Sams-MacBook-Pro.local>
2022-12-16 18:07:08 -0500

    allow float for height

--- FitnessRepository/Models/UserHeight.cs
+++ FitnessRepository/Models/UserHeight.cs
@@ -5,5 +5,5 @@ public sealed class UserHeight
     public long Id { get; set; }
     public DateTime Created { get; set; }
     public Guid UserId { get; set; }
-    public int Height { get; set; }
+    public float Height { get; set; }
 }
