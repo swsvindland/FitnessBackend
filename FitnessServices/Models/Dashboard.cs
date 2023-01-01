9b9b714568422fe9b344ef91fd920ff37a81f233
Sam Svindland <sam@Sams-MacBook-Pro.local>
2023-01-01 12:40:22 -0500

    update dashboard endpoint to support todo dashboard

--- FitnessServices/Models/Dashboard.cs
+++ FitnessServices/Models/Dashboard.cs
@@ -3,8 +3,13 @@ namespace FitnessServices.Models;
 public class Dashboard
 {
     public bool AddWeight { get; set; }
+    public bool WeightAdded { get; set; }
     public bool AddBodyMeasurements { get; set; }
+    public bool BodyMeasurementsAdded { get; set; }
     public bool AddHeight { get; set; }
+    public bool HeightAdded { get; set; }
     public bool AddBloodPressure { get; set; }
+    public bool BloodPressureAdded { get; set; }
     public bool DoWorkout { get; set; }
+    public bool WorkoutAdded { get; set; }
 }
