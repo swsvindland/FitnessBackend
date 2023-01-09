020544af6d16823b12a9412fb49f2c9c3ed850c7
Sam Svindland <sam@Sams-MacBook-Pro.local>
2023-01-09 07:51:34 -0500

    update min version

--- FitnessApi/Controllers/VersionController.cs
+++ FitnessApi/Controllers/VersionController.cs
@@ -30,7 +30,7 @@ public sealed class VersionController
             return new UnauthorizedResult();
         }
 
-        const int minVersion = 30;
+        const int minVersion = 31;
 
         return new OkObjectResult(minVersion);
     }
