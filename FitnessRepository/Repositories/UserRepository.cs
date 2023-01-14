9c87676cf9893512f5adeb34d7e4b34649d37de2
Sam Svindland <sam@Sams-MacBook-Pro.local>
2023-01-14 12:40:39 -0500

    delete progress photos

--- FitnessRepository/Repositories/UserRepository.cs
+++ FitnessRepository/Repositories/UserRepository.cs
@@ -70,6 +70,7 @@ public sealed class UserRepository : IUserRepository
         _context.UserBody.RemoveRange(_context.UserBody.Where(e => e.UserId == userId));
         _context.UserBloodPressure.RemoveRange(_context.UserBloodPressure.Where(e => e.UserId == userId));
         _context.UserCustomMacros.RemoveRange(_context.UserCustomMacros.Where(e => e.UserId == userId));
+        _context.ProgressPhoto.RemoveRange(_context.ProgressPhoto.Where(e => e.UserId == userId));
 
         await _context.SaveChangesAsync();
     }
