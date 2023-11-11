9565e2ae69208dbad8fef678b34b10325e697d5c
Sam Svindland <swsvindland@gmail.com>
2023-11-10 21:03:18 -0500

    remove auth from min version

--- FitnessApi/Controllers/VersionController.cs
+++ FitnessApi/Controllers/VersionController.cs
@@ -1,4 +1,3 @@
-using FitnessServices.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Functions.Worker;
@@ -8,12 +7,10 @@ namespace FitnessApi.Controllers;
 
 public sealed class VersionController
 {
-    private readonly IAuthService _authService;
     private readonly ILogger<VersionController> _logger;
 
-    public VersionController(IAuthService authService, ILogger<VersionController> logger)
+    public VersionController(ILogger<VersionController> logger)
     {
-        _authService = authService;
         _logger = logger;
     }
 
@@ -22,13 +19,6 @@ public sealed class VersionController
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
         HttpRequest req)
     {
-        var authed = _authService.CheckAuth(req);
-
-        if (authed == false)
-        {
-            return new UnauthorizedResult();
-        }
-
         const int minVersion = 34;
 
         return new OkObjectResult(minVersion);
